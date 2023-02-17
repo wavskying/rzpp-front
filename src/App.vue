63de1589fac76525206c23204e36be149b785839
wavskying <2817253626@qq.com>
2023-02-17 21:30:12 +0800

    首页框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" style="margin: 0">
     <router-view/>
   </div>
 </template>
@@ -17,6 +17,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+html,body,#app,.el-container{
+  /*设置内部填充为0，几个布局元素之间没有间距*/
+  padding: 0px;
+  /*外部间距也是如此设置*/
+  margin: 0px;
+  /*统一设置高度为100%*/
+  height: 100%;
 }
 </style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,15 +1,287 @@
 <template>
- <div>
-   这里是首页
- </div>
+  <body style="margin: 0">
+  <el-container style="height: 1000px">
+    <el-header style="background-color: #202329;height: 55px;padding: 5px">
+      <div id="header" style="background-color: #202329">
+        <span data-v-6c36d3c0="" class="el-avatar el-avatar--circle"><img
+          src="../assets/head.png" style="object-fit: cover;"></span>
+      </div>
+    </el-header>
+    <el-main style="background-color: white;height: 145px;padding: 0">
+      <el-row style="height: 90px">
+        <el-col :span="1" style="height: 90px"></el-col>
+        <el-col :span="12" style="height: 90px">
+          <div style="margin-top: 15px;">
+            <el-input id="select" placeholder="搜索职位" v-model="select" style="height: 60px;font-size: 30px">
+              <el-button slot="append" icon="el-icon-search" style="height: 60px"></el-button>
+            </el-input>
+          </div>
+        </el-col>
+        <el-col :span="2" style="height: 90px"></el-col>
+        <el-col :span="6" style="height: 90px">
+
+        </el-col>
+      </el-row>
+      <el-row style="height: 60px">
+        <el-col :span="24" style="height: 60px">
+          <div class="block">
+
+            <el-cascader
+              placeholder="选择职位类型"
+              v-model="value"
+              :options="options"
+              :props="{ expandTrigger: 'hover' }"
+              @change="handleChange"></el-cascader>
+          </div>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer style="background-color: #eef7f9;height: 788px">
+
+    </el-footer>
+  </el-container>
+  </body>
 </template>
 
 <script>
+import avatarURL from '../assets/head.png'
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      avatarURL: avatarURL,
+      select: '',
+      value: [],
+      options: [{
+        value: 'zhinan',
+        label: '指南',
+        children: [{
+          value: 'shejiyuanze',
+          label: '设计原则',
+          children: [{
+            value: 'yizhi',
+            label: '一致'
+          }, {
+            value: 'fankui',
+            label: '反馈'
+          }, {
+            value: 'xiaolv',
+            label: '效率'
+          }, {
+            value: 'kekong',
+            label: '可控'
+          }]
+        }, {
+          value: 'daohang',
+          label: '导航',
+          children: [{
+            value: 'cexiangdaohang',
+            label: '侧向导航'
+          }, {
+            value: 'dingbudaohang',
+            label: '顶部导航'
+          }]
+        }]
+      }, {
+        value: 'zujian',
+        label: '组件',
+        children: [{
+          value: 'basic',
+          label: 'Basic',
+          children: [{
+            value: 'layout',
+            label: 'Layout 布局'
+          }, {
+            value: 'color',
+            label: 'Color 色彩'
+          }, {
+            value: 'typography',
+            label: 'Typography 字体'
+          }, {
+            value: 'icon',
+            label: 'Icon 图标'
+          }, {
+            value: 'button',
+            label: 'Button 按钮'
+          }]
+        }, {
+          value: 'form',
+          label: 'Form',
+          children: [{
+            value: 'radio',
+            label: 'Radio 单选框'
+          }, {
+            value: 'checkbox',
+            label: 'Checkbox 多选框'
+          }, {
+            value: 'input',
+            label: 'Input 输入框'
+          }, {
+            value: 'input-number',
+            label: 'InputNumber 计数器'
+          }, {
+            value: 'select',
+            label: 'Select 选择器'
+          }, {
+            value: 'cascader',
+            label: 'Cascader 级联选择器'
+          }, {
+            value: 'switch',
+            label: 'Switch 开关'
+          }, {
+            value: 'slider',
+            label: 'Slider 滑块'
+          }, {
+            value: 'time-picker',
+            label: 'TimePicker 时间选择器'
+          }, {
+            value: 'date-picker',
+            label: 'DatePicker 日期选择器'
+          }, {
+            value: 'datetime-picker',
+            label: 'DateTimePicker 日期时间选择器'
+          }, {
+            value: 'upload',
+            label: 'Upload 上传'
+          }, {
+            value: 'rate',
+            label: 'Rate 评分'
+          }, {
+            value: 'form',
+            label: 'Form 表单'
+          }]
+        }, {
+          value: 'data',
+          label: 'Data',
+          children: [{
+            value: 'table',
+            label: 'Table 表格'
+          }, {
+            value: 'tag',
+            label: 'Tag 标签'
+          }, {
+            value: 'progress',
+            label: 'Progress 进度条'
+          }, {
+            value: 'tree',
+            label: 'Tree 树形控件'
+          }, {
+            value: 'pagination',
+            label: 'Pagination 分页'
+          }, {
+            value: 'badge',
+            label: 'Badge 标记'
+          }]
+        }, {
+          value: 'notice',
+          label: 'Notice',
+          children: [{
+            value: 'alert',
+            label: 'Alert 警告'
+          }, {
+            value: 'loading',
+            label: 'Loading 加载'
+          }, {
+            value: 'message',
+            label: 'Message 消息提示'
+          }, {
+            value: 'message-box',
+            label: 'MessageBox 弹框'
+          }, {
+            value: 'notification',
+            label: 'Notification 通知'
+          }]
+        }, {
+          value: 'navigation',
+          label: 'Navigation',
+          children: [{
+            value: 'menu',
+            label: 'NavMenu 导航菜单'
+          }, {
+            value: 'tabs',
+            label: 'Tabs 标签页'
+          }, {
+            value: 'breadcrumb',
+            label: 'Breadcrumb 面包屑'
+          }, {
+            value: 'dropdown',
+            label: 'Dropdown 下拉菜单'
+          }, {
+            value: 'steps',
+            label: 'Steps 步骤条'
+          }]
+        }, {
+          value: 'others',
+          label: 'Others',
+          children: [{
+            value: 'dialog',
+            label: 'Dialog 对话框'
+          }, {
+            value: 'tooltip',
+            label: 'Tooltip 文字提示'
+          }, {
+            value: 'popover',
+            label: 'Popover 弹出框'
+          }, {
+            value: 'card',
+            label: 'Card 卡片'
+          }, {
+            value: 'carousel',
+            label: 'Carousel 走马灯'
+          }, {
+            value: 'collapse',
+            label: 'Collapse 折叠面板'
+          }]
+        }]
+      }, {
+        value: 'ziyuan',
+        label: '资源',
+        children: [{
+          value: 'axure',
+          label: 'Axure Components'
+        }, {
+          value: 'sketch',
+          label: 'Sketch Templates'
+        }, {
+          value: 'jiaohu',
+          label: '组件交互文档'
+        }]
+      }],
+    }
+  }
 }
 </script>
 
 <style scoped>
+.el-avatar {
+  display: inline-block;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  text-align: center;
+  overflow: hidden;
+  color: #fff;
+  background: #C0C4CC;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+}
 
+.el-avatar--circle {
+  border-radius: 50%;
+}
+
+.el-select .el-input {
+  height: 60px;
+  width: 130px;
+}
+
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
+
+/deep/ .el-input__inner {
+  height: 60px;
+}
 </style>
